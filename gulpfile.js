e774c3215e93a25a2489219430b07a0f598c748f
Mélina Marignale <melina@marignale.com>
2015-07-12 20:28:18 +0200

    gulpfile is ok. i think.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,65 @@
  * Created by melinamarignale on 03/07/15.
  */
 
-// Include gulp
-var gulp = require('gulp');
+// Include gulp + plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
 
+// CSS watch
+gulp.task('styles', function() {
+    return sass('src/styles/main.scss', { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
 
-// SASS watch
-var sass = require('gulp-ruby-sass');
+// JS watch
+gulp.task('scripts', function() {
+    return gulp.src('src/scripts/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
 
-gulp.task('sass', function () {
-    return sass('source/')
-        .on('error', function (err) {
-            console.error('Error!', err.message);
-        })
-        .pipe(gulp.dest('result'));
+// Clean everything
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts');
+});
+
+// Big Brother is watching you!
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch('src/styles/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+    // Create LiveReload server
+    livereload.listen();
+
+    // Watch any files in dist/, reload on change
+    gulp.watch(['dist/**']).on('change', livereload.changed);
+
 });
