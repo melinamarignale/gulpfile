f79a28b9623bebed61795428614ce2f2af444b1c
Mélina Marignale <melina@marignale.com>
2015-07-12 19:07:18 +0200

    install dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,15 @@
 
 // Include gulp
 var gulp = require('gulp');
+
+
+// SASS watch
+var sass = require('gulp-ruby-sass');
+
+gulp.task('sass', function () {
+    return sass('source/')
+        .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(gulp.dest('result'));
+});
